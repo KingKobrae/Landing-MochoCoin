0c4b2e5b12c2f0fa32ea4c5cb27e9f6ddcc4a459
KingKobrae <jonathanjf250405@gmail.com>
2025-06-14 17:40:53 -0400

    inicio del proyecto
    dando palos de ciego hasta resolver

---
+++ MochoCoin/src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+createRoot(document.getElementById('root')).render(
+  <article>
+    <H1>Pepe</H1>
+  </article>
+)
