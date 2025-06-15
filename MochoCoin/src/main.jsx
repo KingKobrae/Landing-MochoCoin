6b13fa34175cf8e22afc2a7c635dce1e251ba023
KingKobrae <jonathanjf250405@gmail.com>
2025-06-15 15:15:40 -0400

    "avances"
    No hicimos casi nada xD

--- MochoCoin/src/main.jsx
+++ MochoCoin/src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <article>
-    <H1>Pepe</H1>
-  </article>
+  
 )
